aabf85e66b028299ec2b7e9ced5de56e82135452
ran wang <wang.ran.henri@gmail.com>
2020-05-16 17:34:37 -0400

    text navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,26 +1,26 @@
 import React from 'react'
+import { TEXT_NAVBAR } from '../util/variables'
 
 export default function Navbar () {
+    const TEXT = TEXT_NAVBAR
+
     return (
         <div>
-            <nav class='navbar navbar-expand-lg navbar-light bg-light'>
-                <a class='navbar-brand' href='#'>Navbar</a>
-                <button class='navbar-toggler' type='button' data-toggle='collapse' data-target='#navbarNav' aria-controls='navbarNav' aria-expanded='false' aria-label='Toggle navigation'>
-                    <span class='navbar-toggler-icon' />
+            <nav className='navbar navbar-expand-lg navbar-light bg-light'>
+                <a className='navbar-brand' href='#'>{TEXT.TITLE}</a>
+                <button className='navbar-toggler' type='button' data-toggle='collapse' data-target='#navbarNav' aria-controls='navbarNav' aria-expanded='false' aria-label='Toggle navigation'>
+                    <span className='navbar-toggler-icon' />
                 </button>
-                <div class='collapse navbar-collapse' id='navbarNav'>
-                    <ul class='navbar-nav'>
-                        <li class='nav-item active'>
-                            <a class='nav-link' href='#'>Home <span class='sr-only'>(current)</span></a>
+                <div className='collapse navbar-collapse' id='navbarNav'>
+                    <ul className='navbar-nav'>
+                        <li className='nav-item active'>
+                            <a className='nav-link' href='#'>{TEXT.HOME} <span className='sr-only'>(current)</span></a>
                         </li>
-                        <li class='nav-item'>
-                            <a class='nav-link' href='#'>Features</a>
+                        <li className='nav-item'>
+                            <a className='nav-link' href='#'>{TEXT.NEW_QUESTION}</a>
                         </li>
-                        <li class='nav-item'>
-                            <a class='nav-link' href='#'>Pricing</a>
-                        </li>
-                        <li class='nav-item'>
-                            <a class='nav-link disabled' href='#'>Disabled</a>
+                        <li className='nav-item'>
+                            <a className='nav-link' href='#'>{TEXT.LEADER_BOARD}</a>
                         </li>
                     </ul>
                 </div>
