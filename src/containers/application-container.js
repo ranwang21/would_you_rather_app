b64ad4d516119da0571e777afe2b7d63e13fe5ef
ran wang <wang.ran.henri@gmail.com>
2020-05-16 17:21:10 -0400

    add constraints

--- src/containers/application-container.js
+++ src/containers/application-container.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
+import Navbar from '../components/navbar'
 
 export default class ApplicationContainer extends Component {
     render () {
         return (
             <div>
-                <h2>This is app container</h2>
+                <Navbar />
             </div>
         )
     }
---
+++ src/util/variables.js
@@ -0,0 +1,55 @@
+const TEXT_NAVBAR = {
+    TITLE: 'Would You Rather',
+    HOME: 'Home',
+    NEW_QUESTION: 'New Question',
+    LEADER_BOARD: 'Leader Board',
+    LOG_IN: 'Log in',
+    LOG_OUT: 'Log out'
+}
+
+const TEXT_QUESTION_PAGE = {
+    WOULD_YOU_RATHER: 'Would You Rather...',
+    ASKS: 'Asks',
+    SUBMIT: 'Submit',
+    OUT_OF: ' out of ',
+    VOTES: ' votes',
+    YOUR_VOTE: 'Your vote'
+}
+
+const TEXT_NEW_QUESTION = {
+    CREATE_NEW_QUESTION: 'Create New Question',
+    WOULD_YOUR_RATHER: 'Would you rather...',
+    PLACEHOLDER_OPTION1: 'Enter option one text here',
+    PLACEHOLDER_OPTION2: 'Enter option two text here',
+    OR: 'Or',
+    SUBMIT: 'Submit'
+}
+
+const TEXT_LEADERBOARD_PAGE = {
+    ANSWERED_QUESTIONS: 'Answered Questions',
+    CREATED_QUESTIONS: 'Create Questions',
+    SCORE: 'Score'
+}
+
+const TEXT_LOG_IN_PAGE = {
+    WELCOME: 'Welcome to the Would You Rather App!',
+    SIGN_IN: 'Sign in',
+    PLACEHOLDER_USER: 'Select User',
+    SIGN_IN_BUTTON: 'Sign In'
+}
+
+const TEXT_HOME_PAGE = {
+    ASKS: 'Asks',
+    VIEW_POLL: 'View Poll',
+    ANSWERED_QUESTIONS: 'Answered Questions',
+    UNANSWERED_QUESTIONS: 'Unanswered Questions'
+}
+
+export {
+    TEXT_NAVBAR,
+    TEXT_QUESTION_PAGE,
+    TEXT_NEW_QUESTION,
+    TEXT_LEADERBOARD_PAGE,
+    TEXT_LOG_IN_PAGE,
+    TEXT_HOME_PAGE
+}
