b0e6f7d90fd8bc1501a4b9efe84208340a6a921b
ran wang <wang.ran.henri@gmail.com>
2020-05-17 13:10:03 -0400

    install redux react redux

---
+++ src/components/question.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Question () {
+    return (
+        <div>
+            <h2>This is question</h2>
+        </div>
+    )
+}
--- src/containers/application-container.js
+++ src/containers/application-container.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react'
 import Navbar from '../components/navbar'
-import { Route } from 'react-router-dom'
+import { Route, Router } from 'react-router-dom'
 import Login from '../containers/login'
 import NewQuestion from '../components/new-question'
 import LeaderBoard from '../components/leader-board'
+import Question from '../components/question'
 
 export default class ApplicationContainer extends Component {
     render () {
         return (
             <div>
                 <Navbar />
-                <Route path='/home' component={Login} />
-                <Route path='/new-question' component={NewQuestion} />
-                <Route path='/leader-board' component={LeaderBoard} />
+                <Router>
+                    <Route path='/home' exact component={Login} />
+                    <Route path='/new-question' component={NewQuestion} />
+                    <Route path='/leader-board' component={LeaderBoard} />
+                    <Route path='/question' component={Question} />
+                </Router>
+
             </div>
         )
     }
