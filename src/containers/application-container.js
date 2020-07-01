1792ec99cd3a5e8c8c011e11f170958652e11470
ran wang <wang.ran.henri@gmail.com>
2020-07-01 19:08:19 -0400

    login page component

--- src/containers/application-container.js
+++ src/containers/application-container.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import Navbar from '../components/navbar'
 import { Route } from 'react-router-dom'
 import Login from '../containers/login'
-import NewQuestion from '../components/new-question'
+import NewQuestion from '../containers/new-question'
 import LeaderBoard from '../components/leader-board'
 import Question from '../components/question'
 
@@ -10,7 +10,13 @@ export default class ApplicationContainer extends Component {
     render () {
         return (
             <div>
+
                 <Navbar />
+                <div className='container'>
+                    <Route exact path='/' component={Login} />
+                    <Route exact path='/new-question' component={Question} />
+                    <Route exact path='/leader-board' component={LeaderBoard} />
+                </div>
             </div>
         )
     }
--- src/containers/login.js
+++ src/containers/login.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react'
+import { TEXT_LOG_IN_PAGE } from '../util/variables'
 
 export default class Login extends Component {
     render () {
         return (
-            <div>
-                <h2>This is login</h2>
+            <div className='mt-4'>
+                <h2 className='text-center'>{TEXT_LOG_IN_PAGE.WELCOME}</h2>
+                <form>
+                    <div className='form-group'>
+                        <label for='users'>{TEXT_LOG_IN_PAGE.SIGN_IN}</label>
+                        <select className='form-control' id='users'>
+                            <option>1</option>
+                            <option>2</option>
+                            <option>3</option>
+                            <option>4</option>
+                        </select>
+                    </div>
+                    <button type='submit' className='btn btn-primary'>{TEXT_LOG_IN_PAGE.SIGN_IN_BUTTON}</button>
+                </form>
             </div>
         )
     }
